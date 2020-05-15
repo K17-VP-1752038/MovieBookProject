872cf74d119d60e6bc92fa82f86eb76f60baea4e
K17-VP-1752048 <40013974+K17-VP-1752048@users.noreply.github.com>
2020-05-15 09:42:15 +0700

    Trang update
    update

--- src/Frames/MainFrame.java
+++ src/Frames/MainFrame.java
@@ -1,19 +1,29 @@
 package Frames;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
+import javax.swing.BoxLayout;
 
 public class MainFrame extends JFrame {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -35,12 +45,68 @@ public class MainFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public MainFrame() {
+		setSize(800,600);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		setLocationRelativeTo(null);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBackground(new Color(0, 0, 0));
+		setJMenuBar(menuBar);
+		
+		JMenu mnHome = new JMenu("HOME");
+		mnHome.setFont(new Font("Segoe UI Black", Font.PLAIN, 12));
+		mnHome.setForeground(new Color(255, 0, 0));
+		mnHome.setBackground(new Color(0, 0, 0));
+		menuBar.add(mnHome);
+		
+		JMenu mnNewMenu_1 = new JMenu("TVshows");
+		mnNewMenu_1.setBackground(new Color(0, 0, 0));
+		mnNewMenu_1.setForeground(new Color(255, 0, 0));
+		mnNewMenu_1.setFont(new Font("Segoe UI Black", Font.PLAIN, 12));
+		menuBar.add(mnNewMenu_1);
+		
+		JMenu mnNewMenu_2 = new JMenu("Movies");
+		mnNewMenu_2.setFont(new Font("Segoe UI Black", Font.PLAIN, 12));
+		mnNewMenu_2.setForeground(new Color(255, 0, 0));
+		mnNewMenu_2.setBackground(new Color(0, 0, 0));
+		menuBar.add(mnNewMenu_2);
+		
+		JPanel panelRightTop = new JPanel();
+		panelRightTop.setBackground(new Color(0, 0, 0));
+		FlowLayout flowLayout = (FlowLayout) panelRightTop.getLayout();
+		flowLayout.setAlignment(FlowLayout.TRAILING);
+		menuBar.add(panelRightTop);
+		
+		JTextField txtSearch = new JTextField();
+		panelRightTop.add(txtSearch);
+		txtSearch.setColumns(10);
+		
+		JButton btnSearch = new JButton("");
+		btnSearch.setPreferredSize(new Dimension(30,30));
+		btnSearch.setFocusPainted(true);
+		btnSearch.setContentAreaFilled(false);
+		btnSearch.setBorderPainted(false);
+		btnSearch.setIcon(new ImageIcon(new ImageIcon("icon\\iconsearch.png").getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT)));
+		btnSearch.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+		panelRightTop.add(btnSearch);
+		
+		JLabel lblUser = new JLabel("");
+		lblUser.setIcon(new ImageIcon(new ImageIcon("icon\\iconuser.png").getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT)));
+		lblUser.setForeground(Color.BLACK);
+		panelRightTop.add(lblUser);
+		
+		
+		String[] cols = {"Name", "Age","Gender"};
+		String [][]data = {{"Trang","20","female"},{"Vi","20","female"},{"Nguyet","20","female"}};
+		getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+		table = new JTable(data, cols) {
+			public boolean isCellEditable(int data, int cols) {
+				return false;
+			}
+		};
+		table.setPreferredScrollableViewportSize(new Dimension(450,63));
+		table.setFillsViewportHeight(true);
+		getContentPane().add(table);
 	}
 
 }
