ebc5629598dd97d869d5a35e336c277be61b23e1
1752038_K17VP <K17VP1752038@users.noreply.github.com>
2020-06-06 12:18:01 +0700

    Update FilmLibrary.java
    hulaaaaa

--- src/IOFilmFile/FilmLibrary.java
+++ src/IOFilmFile/FilmLibrary.java
@@ -12,13 +12,17 @@ public class FilmLibrary {
 	private static final String seriesfile="series/Series_DB.txt";
 	private filmReadWrite auth = new filmReadWrite();
 	private ArrayList<Film> films;
-	private ArrayList<String> del_movie_img = new ArrayList<>();
-	private ArrayList<String> del_series_img = new ArrayList<>();	
-	private ArrayList<filmImage> images = new ArrayList<>();
+	private ArrayList<String> del_movie_img;
+	private ArrayList<String> del_series_img;	
+	private ArrayList<filmImage> images;
 	
 	// Constructor
 	public FilmLibrary() {
 		films = new ArrayList<>();
+		del_movie_img = new ArrayList<>();
+		del_series_img = new ArrayList<>();
+		images = new ArrayList<>();
+		
 		films.addAll(auth.readMovieFile(moviesfile));
 		films.addAll(auth.readSeriesFile(seriesfile));
 	}
